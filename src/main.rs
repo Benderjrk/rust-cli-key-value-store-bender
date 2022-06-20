2446bd9784f6a673500059f8d0ebbe7289b348a9
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-06-20 12:52:55 -0400

    Saving single line to db file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
-    let value = arguments.next().unwrap();
+    let key = arguments.next().expect("Key was not there");
+    let value = arguments.next().expect("Value was not there");
     println!("The key is '{}' and the value is '{}'", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
 }
+
+struct Database {}
